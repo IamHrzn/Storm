cee0d9c4cd9e29fb0135b71449864e2f4a82bfb6
Limo <99534559+IamHrzn@users.noreply.github.com>
2024-06-18 17:19:27 +0200

    StormClient lua build

---
+++ Client.lua
@@ -0,0 +1,12 @@
+--[[
+    Developed by @IAMHRZN
+    Discord: .hrzn. 
+
+    Developed by @MASTEROOGWAY
+    Discord: realmasteroogway
+
+    official StormHub discord: https://discord.gg/kRBuUYMYzs
+    ^^ If you wish to discuss, purchase, report an error, please go here.
+]]
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/fc45ea93ac3260dcbedce60c0af39c86.lua"))()
