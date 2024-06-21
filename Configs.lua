e726e54d64b76d2a41ca089f74018a82bc6d71a4
Limo <99534559+IamHrzn@users.noreply.github.com>
2024-06-21 11:27:10 +0200

    Update Configs.lua

--- Configs.lua
+++ Configs.lua
@@ -1,4 +1,4 @@
-if workspace:FindFirstChild("nonLiving") then
+
   getgenv().autoparryModule = {}
   getgenv().autoparryModule.parryAbilities = {"archer_spin","throw_firecrackers","archer_rapid_shot","ghoststrike","bomb","shield_bash","royal_counter_use","noble","sumo_barrage","twin_rampage","earth_shatter","headsmash","boxersecond","boxer3rd","hip_fire","quick_draw","thousand_cuts","Swing","aerial","gp","gambler_fourth","gambler_second","wraith_fourth","wraith_light","wrath_eletric","wraith_first"}
   getgenv().autoparryModule.noParry = {"Climbing archer_spin","Foot","Jump","dash","Dash","Wall_jump","KO_Spin_Loop","Spin_Start","lightarcSFX"}
@@ -8,4 +8,4 @@ if workspace:FindFirstChild("nonLiving") then
     wait()
   end
   return getgenv().autoparryModule 
-end
+
