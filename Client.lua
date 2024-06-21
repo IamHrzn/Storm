abb0033315556f5ee5c8f9bd6df10a11195c1c20
Limo <99534559+IamHrzn@users.noreply.github.com>
2024-06-21 21:11:58 +0200

    Update Client.lua

--- Client.lua
+++ Client.lua
@@ -9,4 +9,4 @@
     ^^ If you wish to discuss, purchase, report an error, please go here.
 ]]
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/fc45ea93ac3260dcbedce60c0af39c86.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b53f6bdba4c684b53a8ce9596ba5260.lua"))()
